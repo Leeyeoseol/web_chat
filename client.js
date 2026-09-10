966c06a8a4ce52dd9b1e11467c476c31344ddc8d
Leeyeoseol <iyeoseol096@gmail.com>
2026-09-10 17:19:52 +0900

    2-4. UI 이벤트 처리

--- client.js
+++ client.js
@@ -1,1 +1,22 @@
-console.log("client.js");
+const chatForm = document.getElementById("chat-form");
+const chatInput = document.getElementById("chat-input");
+const messages = document.getElementById("messages");
+
+function addMessage(messageText) {
+    const message = document.createElement("li");
+    message.textContent = messageText;
+    messages.appendChild(message);
+}
+
+chatForm.addEventListener("submit", (event) => {
+    event.preventDefault();//화면 깜박거리는 기본 동작 막음
+
+    const messageText = chatInput.value.trim();//앞 뒤 여백 자르기
+
+    if (!messageText) return;//메세지 없으면 그냥 return
+
+    addMessage(messageText);
+
+    chatInput.value = "";
+    chatInput.focus();
+});
